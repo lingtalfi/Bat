4045a81df37fdae5a0ee82c9ec66dfca1f1ebf09
lingtalfi <lingtalfi@gmail.com>
2015-11-10 19:31:28 +0100

    StringTool:htmlAttributes add test

---
+++ btests/StringTool/htmlAttributes/stringTool.htmlAttributes.test.php
@@ -0,0 +1,39 @@
+<?php
+
+use Bat\FileSystemTool;
+use Bat\StringTool;
+use PhpBeast\AuthorTestAggregator;
+use PhpBeast\PrettyTestInterpreter;
+
+require_once "bigbang.php";
+
+
+//------------------------------------------------------------------------------/
+// EXHAUSTING TEST DEMO
+//------------------------------------------------------------------------------/
+$agg = AuthorTestAggregator::create();
+
+$a = [
+    [],
+    ['required'],
+    ['class' => 'doo'],
+    ['class' => 'doo', 'required', "id" => "foo"],
+    ['src' => '/cet-été/dummy'],
+];
+
+$b = [
+    '',
+    ' required',
+    ' class="doo"',
+    ' class="doo" required id="foo"',
+    ' src="/cet-été/dummy"',
+];
+
+
+$agg->addTestsByColumn($a, $b, function ($value, $expected, &$msg) {
+    $res = StringTool::htmlAttributes($value);
+    return ($expected === $res);
+});
+
+
+PrettyTestInterpreter::create()->execute($agg);
